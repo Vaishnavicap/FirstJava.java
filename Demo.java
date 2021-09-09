5bf3f16121f2760ceb80b6dadceaf61e9a524760
Vaishnavicap <90391261+Vaishnavicap@users.noreply.github.com>
2021-09-09 17:06:58 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package demo;
 
 
 public class Demo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Hello Java");
         System.out.println("I'm Vaishnavi Satish Kamble");
