49e515a8f1e64f56ae272dcb388273c40f07878b
Vaishnavicap <90391261+Vaishnavicap@users.noreply.github.com>
2021-09-09 17:14:19 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -5,7 +5,7 @@ public class Demo {
         System.out.println("Hello Java");
         System.out.println("I'm Vaishnavi Satish Kamble");
         System.out.println("I'm a Third Year Student Of Computer Engineering at MMIT College ");
-        System.out.println("Today Onwards I'm Going to Start Learning Java so see you in There.");
+        System.out.println("Today Onwards I'm Going to Start Learning Java so see you in there.");
     }
     
 }
